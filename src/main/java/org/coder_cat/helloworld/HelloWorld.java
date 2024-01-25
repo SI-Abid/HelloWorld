9390ce1fb06ddc26096558e3038032fdfa612a47
Saiham Islam Abid <siaabid5323@gmail.com>
2024-01-25 23:09:13 +0600

    Initial commit with bug fix

--- src/main/java/org/coder_cat/helloworld/HelloWorld.java
+++ src/main/java/org/coder_cat/helloworld/HelloWorld.java
@@ -2,6 +2,7 @@ package org.coder_cat.helloworld;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import org.coder_cat.helloworld.commands.RandomTeleport;
+import org.coder_cat.helloworld.listeners.PlayerConsumeListener;
 import org.coder_cat.helloworld.listeners.PlayerJoinListener;
 
 public final class HelloWorld extends JavaPlugin {
@@ -16,6 +17,8 @@ public final class HelloWorld extends JavaPlugin {
 
         // Event Listeners
         getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
+        getServer().getPluginManager().registerEvents(new PlayerConsumeListener(), this);
+
     }
 
     @Override
